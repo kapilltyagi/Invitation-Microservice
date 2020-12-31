3f64b988048b04fd3f91ed3ed5b7b5710568abd7
kapilltyagi <kapill.tyagi@gmail.com>
2020-12-31 13:03:28 +0530

    sixth commit

--- src/main/java/com/tng/invitation/service/CsvWriterService.java
+++ src/main/java/com/tng/invitation/service/CsvWriterService.java
@@ -5,9 +5,16 @@ import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.StatefulBeanToCsv;
 import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import com.opencsv.exceptions.CsvException;
+import com.tng.invitation.entity.AdminInvitations;
 import com.tng.invitation.entity.InvitationsDTO;
+import com.tng.invitation.repository.InvitationRepository;
+import com.tng.invitation.repository.InvitationRepositoryInterface;
 import com.tng.invitation.utils.ByteArrayInOutStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
@@ -15,10 +22,26 @@ import java.io.ByteArrayInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class CsvWriterService {
+
+    @Autowired
+    private InvitationsValidationService invitationsValidationService;
+
+    @Autowired
+    private InvitationRepositoryInterface invitationRepositoryInterface;
+
+    @Autowired
+    private InvitationRepository invitationRepository;
+
     public Mono<ByteArrayInputStream> generateCsv(List<InvitationsDTO> invitationDtoList) {
         String[] columns = {"firstName", "lastName", "companyName", "email", "countryAbbr", "stateProvinceAbbr", "invitedTo", "result", "message"};
 
@@ -96,4 +119,130 @@ public class CsvWriterService {
         }
         return "CSV Successfully Written";
     }
+
+
+    public Flux<AdminInvitations> getLines(FilePart filePart) {
+        return filePart.content()
+                .map(dataBuffer -> {
+                    byte[] bytes = new byte[dataBuffer.readableByteCount()];
+                    dataBuffer.read(bytes);
+                    DataBufferUtils.release(dataBuffer);
+
+                    return new String(bytes, StandardCharsets.UTF_8);
+                })
+                .map(this::processAndGetLinesAsList)
+
+                .flatMapIterable(Function.identity())
+                .filter(invitation -> invitation.getResult().equals("Success"))
+                .map(data -> {
+                    AdminInvitations invitations = new AdminInvitations();
+                    invitations.setFirstName(data.getFirstName());
+                    invitations.setLastName(data.getLastName());
+                    invitations.setCompanyName(data.getCompanyName());
+                    invitations.setEmail(data.getEmail());
+                    invitations.setCountryAbbr(data.getCountryAbbr());
+                    invitations.setStateProvinceAbbr(data.getStateProvinceAbbr());
+                    invitations.setInvitedTo(data.getInvitedTo());
+                    return invitations;
+                })
+                .flatMap(data -> invitationRepositoryInterface.save(data));
+
+
+    }
+
+    public List<InvitationsDTO> processAndGetLinesAsList(String string) {
+        List<InvitationsDTO> invitationsDTOList = new ArrayList<>();
+
+        Supplier<Stream<String>> streamSupplier = string::lines;
+        List<String> list = streamSupplier.get().collect(Collectors.toList());
+        list.remove(0);
+        for (String s : list) {
+            String[] arr = s.split(",");
+            InvitationsDTO invitationsDTO = new InvitationsDTO();
+            invitationsDTO.setFirstName(arr[0]);
+            invitationsDTO.setLastName(arr[1]);
+            invitationsDTO.setCompanyName(arr[2]);
+            invitationsDTO.setEmail(arr[3]);
+            invitationsDTO.setCountryAbbr(arr[4]);
+            invitationsDTO.setStateProvinceAbbr(arr[5]);
+            invitationsDTO.setInvitedTo(arr[6]);
+            invitationsDTO.setResult("Success");
+
+
+            boolean firstNamePresent = invitationsValidationService.isFirstNamePresent(arr[0]);
+            if (!firstNamePresent) {
+                invitationsDTO.setResult("Error");
+                invitationsDTO.setMessage("First Name is a required field");
+            }
+            boolean lastNamePresent = invitationsValidationService.isLastNamePresent(arr[1]);
+            if (!lastNamePresent) {
+                if (invitationsDTO.getMessage() == null) {
+                    invitationsDTO.setMessage("Last Name is a require field");
+                }
+                invitationsDTO.setResult("Error");
+
+            }
+
+            boolean emailPresent = invitationsValidationService.isEmailPresent(arr[3]);
+            if (!emailPresent) {
+                if (invitationsDTO.getMessage() == null) {
+                    invitationsDTO.setMessage("Email is a require field");
+                }
+                invitationsDTO.setResult("Error");
+
+            }
+
+            boolean isProvinceStateValid = invitationsValidationService.isProvinceStateValid(arr[5]);
+            if (!isProvinceStateValid) {
+                if (invitationsDTO.getMessage() == null) {
+                    invitationsDTO.setMessage("State should be a valid two letter code");
+                }
+                invitationsDTO.setResult("Error");
+
+            }
+
+            boolean isValidEmail = invitationsValidationService.isValidEmail(arr[3]);
+            if (!isValidEmail) {
+                if (invitationsDTO.getMessage() == null) {
+                    invitationsDTO.setMessage("Email is in incorrect format");
+                }
+                invitationsDTO.setResult("Error");
+
+            }
+            String emailFromDB = invitationRepository.findByEmail(arr[3]);
+            if (emailFromDB != null ) {
+                if (invitationsDTO.getMessage() == null) {
+                    invitationsDTO.setMessage("Email already exist");
+                }
+                invitationsDTO.setResult("Error");
+
+            }
+            if(invitationsDTO.getMessage()==null){
+                invitationsDTO.setMessage("");
+            }
+            invitationsDTOList.add(invitationsDTO);
+
+        }
+        //Mono<ByteArrayInputStream> csvServiceOutput = csvWriterService.generateCsv(invitationsDTOList);
+        //invitationsDTOList.stream().forEach(System.out::println);
+        createCsvFromList(invitationsDTOList);
+        //return streamSupplier.get().collect(Collectors.toList());
+        //saving success records in DB
+
+        /*Flux.fromIterable(invitationsDTOList).filter(invitation -> invitation.getResult().equals("Success"))
+                .map(data -> {
+                    AdminInvitations invitations = new AdminInvitations();
+                    invitations.setFirstName(data.getFirstName());
+                    invitations.setLastName(data.getLastName());
+                    invitations.setCompanyName(data.getCompanyName());
+                    invitations.setEmail(data.getEmail());
+                    invitations.setCountryAbbr(data.getCountryAbbr());
+                    invitations.setStateProvinceAbbr(data.getStateProvinceAbbr());
+                    invitations.setInvitedTo(data.getInvitedTo());
+                    return invitations;
+                })
+                .flatMap(data -> invitationRepositoryInterface.save(data));*/
+        return invitationsDTOList;
+    }
+
 }
